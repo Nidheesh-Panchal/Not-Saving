3ac5ba164fff989a2bdfbc5b5c3d803ae2977633
Nidheesh Panchal <nilu.1999@yahoo.com>
2020-02-03 11:17:42 +0530

    service keeps on running in background (mostly)

--- app/src/main/java/com/example/notsaving/MainActivity.java
+++ app/src/main/java/com/example/notsaving/MainActivity.java
@@ -1,28 +1,19 @@
 package com.example.notsaving;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,12 +40,17 @@ public class MainActivity extends AppCompatActivity {
 			Log.d("notsave", "has access");
 		}
 
+		/*Intent broadcastIntent = new Intent();
+		broadcastIntent.setAction("startservice");
+		broadcastIntent.setClass(this, NotService.class);
+		this.sendBroadcast(broadcastIntent);*/
+
 		IntentFilter filter = new IntentFilter();
 		filter.addAction("com.example.notsaving");
-		updateUIReciver = new BroadcastReceiver() {
+		/*updateUIReciver = new BroadcastReceiver() {
 			@Override
 			public void onReceive(Context context, Intent intent) {
-				Log.d("notsave","receiving");
+				*//*Log.d("notsave","receiving");
 				String title=intent.getStringExtra("title");
 //				Log.d("notsave","Title: "+title);
 				String text=intent.getStringExtra("text");
@@ -66,8 +62,8 @@ public class MainActivity extends AppCompatActivity {
 						LinearLayout.LayoutParams.MATCH_PARENT
 				);
 				tv.setLayoutParams(params);
-				noti_list.addView(tv);
-				/*for(int i=0;i<5;i++)
+				noti_list.addView(tv);*//*
+				*//*for(int i=0;i<5;i++)
 				{
 
 					TextView textView = new TextView(MainActivity.this);
@@ -79,16 +75,22 @@ public class MainActivity extends AppCompatActivity {
 					);
 					textView.setLayoutParams(params);
 					noti_list.addView(textView);
-				}*/
+				}*//*
 			}
 		};
-		registerReceiver(updateUIReciver,filter);
+		registerReceiver(updateUIReciver,filter);*/
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		unregisterReceiver(updateUIReciver);
+//		unregisterReceiver(updateUIReciver);
+		/*Intent broadcastIntent = new Intent();
+		broadcastIntent.setAction("restartservice");
+		broadcastIntent.setClass(this, NotService.class);
+		this.sendBroadcast(broadcastIntent);*/
+		IntentFilter filter = new IntentFilter();
+		filter.addAction("com.example.notsaving");
 	}
 
 	public void requestPermission() {
